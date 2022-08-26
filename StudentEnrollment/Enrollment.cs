3aa7225da73cfaf6cde2679da18d17449246e6f2
abenteuerzeit <amroz.muzyka@gmail.com>
2022-08-26 11:17:48 +0200

    Define and use a record type

---
+++ StudentEnrollment/Enrollment.cs
@@ -0,0 +1,6 @@
+﻿using System;
+
+namespace StudentEnrollment
+{
+    record Enrollment(int StudentID, string CourseName, DateOnly DateEnrolled);
+}
--- StudentEnrollment/Program.cs
+++ StudentEnrollment/Program.cs
@@ -6,7 +6,24 @@ namespace StudentEnrollment
     {
         static void doWork()
         {
-            // TODO:
+            var Enrollments = new Enrollment[4];
+            Enrollments[0] = new Enrollment(StudentID: 1, CourseName: "Physics", DateEnrolled: new DateOnly(2021, 07, 20));
+            Enrollments[1] = new Enrollment(StudentID: 1, CourseName: "Chemistry", DateEnrolled: new DateOnly(2021, 07, 20));
+            Enrollments[2] = Enrollments[0];
+            Enrollments[3] = Enrollments[0] with { StudentID = 2 };
+
+            foreach (var Enrollment in Enrollments)
+            {
+                Console.WriteLine($"{Enrollment}");
+            }
+
+            var firstEnrollment = Enrollments[0];
+            foreach (var Enrollment in Enrollments[1..4])
+            {
+                Console.WriteLine($"{firstEnrollment == Enrollment}");
+            }
+
+            //Immutable records, the following statemnt will fail to build: Enrollments[0].DateEnrolled = new DateOnly(2021, 08, 15);
         }
 
         static void Main(string[] args)
